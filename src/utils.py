a3d0f8ac36bf927e759d67432553ddceae745747
Camilo <camilochs@gmail.com>
2024-10-17 21:10:23 +0200

    update

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@
 # Copyright (c) [2024] Camilo Chacón Sartori
 
 import time
+from pathlib import Path
 
 def generate_random_name(prefix, extension):
     timestamp = time.strftime("%Y%m%d_%H%M%S")
@@ -14,6 +15,10 @@ def generate_random_name(prefix, extension):
 
 def write_file(content, prefix, extension="txt"):
     name_file = generate_random_name(prefix, extension)
-    with open(f"outputs/{prefix}/{name_file}", "w") as f:
+    path = Path(f"outputs/{prefix}/")
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+    with open(f"{path.as_posix()}/{name_file}", "w") as f:
         f.write(content)
         print(f"{prefix} generated: outputs/{prefix}/{name_file}")
